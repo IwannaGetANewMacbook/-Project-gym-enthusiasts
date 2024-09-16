308f582c3b13a044e06e4a41e8fbb5c0a1271a65
labtopForSuhyeon <01057557503a@gmail.com>
2024-09-16 20:35:31 +0900

    [UI-WIP] Post.tsx: solve img issue

--- apps/ui/src/components/Post.tsx
+++ apps/ui/src/components/Post.tsx
@@ -1,12 +1,33 @@
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 import Form from 'react-bootstrap/Form';
+import axios from 'axios';
 
 export function Post() {
   const navigate = useNavigate();
   const accessToken = window.localStorage.getItem('accessToken');
+  const env = import.meta.env;
+
+  const [imageFile, setimageFile] = useState<File | null>(null);
+
+  const [title, setTitle] = useState('');
+
+  const [content, setContent] = useState('');
+
+  const handleImgForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setimageFile(e.currentTarget.files[0]);
+  };
+
+  const handleTitleForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value);
+  };
+
+  const handleContentForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(e.currentTarget.value);
+  };
 
   useEffect(() => {
     if (!accessToken) {
@@ -22,25 +43,74 @@ export function Post() {
         justifyContent: 'center',
         alignItems: 'center',
         textAlign: 'left',
+        position: 'relative',
+        marginBottom: '100px',
       }}
     >
       <Card style={{ width: '80%' }}>
         <Form.Group controlId='formFileMultiple' className='mb-3'>
           <Form.Label>사진을 올려주세요.</Form.Label>
-          <Form.Control type='file' multiple />
+          <Form.Control type='file' multiple onChange={handleImgForm} />
         </Form.Group>
         {/* <Card.Img variant='top' src='holder.js/100px180?text=Image cap' /> */}
         <Card.Title>
-          <Form.Control as='textarea' placeholder='제목을 입력해 주세요.' />
+          <Form.Control
+            as='textarea'
+            placeholder='제목을 입력해 주세요.'
+            onChange={handleTitleForm}
+          />
         </Card.Title>
         <Card.Body>
           <Form.Control
             as='textarea'
             placeholder='내용을 입력해 주세요.'
             style={{ height: '300px' }}
+            onChange={handleContentForm}
           />
         </Card.Body>
       </Card>
+      <Button
+        style={{
+          position: 'absolute',
+          bottom: -50,
+        }}
+        variant='dark'
+        type='button'
+        onClick={() => {
+          if (!imageFile) {
+            alert('이미지를 넣어 주십시오.');
+            window.location.reload();
+          }
+
+          const formData = new FormData();
+          formData.append('title', title);
+          formData.append('content', content);
+          formData.append('image', imageFile);
+          axios
+            .post(
+              `${env.VITE_HOST}/posts`,
+              formData,
+
+              {
+                headers: {
+                  Authorization: `Bearer ${accessToken}`,
+                },
+              }
+            )
+            .then((r) => {
+              console.log(r.data);
+              alert('포스팅 완료!');
+              navigate('/');
+            })
+            .catch((e) => {
+              console.log(e.response?.data.message);
+              alert(e.response?.data.message);
+              window.location.reload();
+            });
+        }}
+      >
+        Post
+      </Button>
     </div>
   );
 }
