043b987fafb35f464634509f721f4628ed4c853c
labtopForSuhyeon <01057557503a@gmail.com>
2025-01-27 19:40:13 +0900

    Include dist folder in repository3

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -5,4 +5,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [nodePolyfills(), react()],
+  build: {
+    outDir: 'dist', // 빌드된 파일이 저장되는 폴더
+  },
 });
