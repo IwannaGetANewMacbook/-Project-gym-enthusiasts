764a8e28a05f6850c095a78b47ca025988f3f0a5
labtopForSuhyeon <01057557503a@gmail.com>
2025-01-28 14:24:42 +0900

    Ensure all components are tracked3

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [nodePolyfills(), react()],
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-    },
-  },
-  build: {
-    outDir: 'build', // 빌드된 파일이 저장되는 폴더
-  },
 });
