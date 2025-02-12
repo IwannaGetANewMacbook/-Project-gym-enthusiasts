fd3b1cac7b5e5f4f9997bacb4c964ac2d3dfd90b
labtopForSuhyeon <01057557503a@gmail.com>
2025-02-12 18:06:42 +0900

    [DEPLOYMENT] Commit for error analysis (3).

--- node_modules/api/src/main.ts
+++ node_modules/api/src/main.ts
@@ -10,13 +10,16 @@ async function bootstrap() {
      * For Deployement
      */
     origin: 'https://myproject-ui.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Authorization'],
 
     /**
      * ForLocal
      */
     // origin: 'http://localhost:5173', // 또는 특정 도메인만 허용
 
-    credentials: true,
+    // credentials: true,
   });
 
   // cookie-parser 미들웨어 사용
