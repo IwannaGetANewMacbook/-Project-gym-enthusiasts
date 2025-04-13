b5449ccfe2023442b19657eac1effd0d27d60553
labtopForSuhyeon <01057557503a@gmail.com>
2025-04-13 14:54:17 +0900

    [UI-DONE] Add Privacy Policy & Terms links to app && update social links in Footer.tsx to images.

--- apps/ui/src/components/Footer.tsx
+++ apps/ui/src/components/Footer.tsx
@@ -22,34 +22,68 @@ export function Footer() {
             <Col md={4} className='mb-3'>
               <h5>Follow Us</h5>
               <ul className='list-inline'>
-                <li className='list-inline-item'>
+                <li className='list-inline-item mx-2'>
                   <a
                     href='https://github.com/IwannaGetANewMacbook'
                     className='text-light'
+                    target='_blank'
+                    rel='noopener noreferrer'
                   >
-                    GitHub
+                    <i
+                      className='bi bi-github'
+                      style={{ fontSize: '1.5rem' }}
+                    ></i>
                   </a>
                 </li>
-                <li className='list-inline-item'>
+                <li className='list-inline-item mx-2'>
                   <a
                     href='https://www.linkedin.com/in/%EC%88%98%ED%98%84-%EA%B9%80-bb934b2b6/'
                     className='text-light'
+                    target='_blank'
+                    rel='noopener noreferrer'
                   >
-                    LinkedIn
+                    <i
+                      className='bi bi-linkedin'
+                      style={{ fontSize: '1.5rem' }}
+                    ></i>
                   </a>
                 </li>
-                <li className='list-inline-item'>
+                <li className='list-inline-item mx-2'>
                   <a
                     href='https://www.instagram.com/suhyeon.ts/'
                     className='text-light'
+                    target='_blank'
+                    rel='noopener noreferrer'
                   >
-                    Instagram
+                    <i
+                      className='bi bi-instagram'
+                      style={{ fontSize: '1.5rem' }}
+                    ></i>
                   </a>
                 </li>
               </ul>
             </Col>
           </Row>
           <hr className='bg-light' />
+
+          {/* 정책 링크 섹션 */}
+          <div className='text-center mb-2'>
+            <a
+              href='/privacy-policy.html'
+              className='text-light me-3'
+              style={{ textDecoration: 'underline' }}
+            >
+              개인정보처리방침
+            </a>
+            <a
+              href='/terms-of-service.html'
+              className='text-light'
+              style={{ textDecoration: 'underline' }}
+            >
+              서비스 약관
+            </a>
+          </div>
+
           <div className='text-center'>
             <p className='mb-0'>© 2025 Suhyeon Kim. All Rights Reserved.</p>
           </div>
